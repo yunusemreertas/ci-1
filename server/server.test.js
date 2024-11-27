bd70a8853f5397c7969f03c34ca4e34035d407a9
Justin Gottschalk <justindevs@gmail.com>
2024-11-26 18:46:11 -0600

    Fix build

---
+++ server/server.test.js
@@ -0,0 +1,13 @@
+describe('SERVER TEST SUITE', () => {
+
+    describe('Running a test', () => {
+
+        it('should pass when running a test', () => {
+            const expected = false;
+
+            expect(false).toEqual(expected)
+        });
+
+    });
+
+});
